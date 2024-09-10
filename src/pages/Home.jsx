2f788b581065ce014e48789af0771499254665f3
Patrik Demel <d20133@oaopava.cz>
2024-09-10 20:05:26 +0200

    home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,27 +27,27 @@ function Home() {
       <Navbar />
 
       {/* Hero section */}
-      <section className="bg-darkerSectionColor pt-5">
+      <section className="bg-darkerSectionColor pt-5 w-full px-5 md:px-0">
         {/* Responsive container */}
-        <div className="container mx-auto flex justify-between">
+        <div className="container mx-auto flex flex-col md:flex-row md:justify-between">
           {/* Left side */}
-          <div className="flex flex-col w-[55%] mt-20">
+          <div className="flex flex-col w-full md:w-[55%] mt-20">
             <p className="text-subheadingColor text-paragraphSize font-bold">
               Nemůžeš zachránit všechny...
             </p>
-            <h1 className="text-heading1Size font-bold -mt-2">
+            <h1 className="text-heading1Size font-bold mt-1 leading-tight md:leading-none">
               Ale můžeš dát <span className="text-mainColor">domov</span>{' '}
               jednomu.
             </h1>
-            <p className="text-subheadingColor font-semibold mb-10 text-paragraphSize mt-2">
+            <p className="text-subheadingColor font-semibold mb-10 text-paragraphSize mt-6 md:mt-4">
               Snažíme se pomáhat v případech týrání zvířat či zanedbání péče, a
               to většinou na základě informací těch z Vás, kteří si všímáte, co
               se děje ve Vašem okolí. Podáváme podněty k šetření případů na
               příslušné kompetentní orgány, případně poskytujeme rady nebo
               doporučení ke konkrétním případům...
             </p>
-            <Link to="/o-nas">
-              <div className="py-2 px-14 border-[4px] border-mainColor hover:border-mainHoverColor duration-200 transition-all rounded-full inline-block">
+            <Link to="/o-nas" className="mb-20">
+              <div className="py-2 px-10 md:px-14 border-[4px] border-mainColor hover:border-mainHoverColor duration-200 transition-all rounded-full inline-block">
                 <div className="flex items-center">
                   <FaArrowRight />
                   <p className="font-bold ms-2 text-paragraphSize">
@@ -58,11 +58,11 @@ function Home() {
             </Link>
           </div>
           {/* Right side */}
-          <div className="flex justify-end w-[45%]">
+          <div className="flex justify-center md:justify-end w-full md:w-[45%] mt-10 md:mt-0">
             <img
               src="/src/assets/hero-img.png"
               alt="Obrázek pejska"
-              className="w-3/4"
+              className="w-3/5 md:w-full lg:w-[80%] aspect-square"
             />
           </div>
         </div>
@@ -72,46 +72,61 @@ function Home() {
       <section>
         {/* Responsive container */}
         <div className="container mx-auto flex flex-col items-center py-10">
-          <h2 className="text-heading2Size font-bold">Přispět</h2>
+          <h2 className="text-heading2Size font-bold text-center">Přispět</h2>
           {/* Boxes container */}
-          <div className="flex w-full justify-center mt-7">
+          <div className="flex flex-col items-stretch md:justify-center md:flex-row mt-7">
             {/* Box */}
-            <div className="bg-white flex flex-col items-center py-8 px-20 shadow-custom mx-5">
-              <h3 className="text-heading3Size font-bold mb-3">QR kód</h3>
-              <img src="/src/assets/qr-placeholder.png" alt="QR kód" />
+            <div className="bg-white flex flex-col items-center py-8 px-10 md:px-20 shadow-custom mx-5 my-2 md:my-0">
+              <h3 className="text-heading3Size font-bold mb-3 text-center">
+                QR kód
+              </h3>
+              <img
+                src="/src/assets/qr-placeholder.png"
+                alt="QR kód"
+                className="w-3/4"
+              />
             </div>
             {/* Box */}
-            <div className="bg-white flex flex-col items-center py-8 px-20 shadow-custom mx-5">
-              <h3 className="text-heading3Size font-bold mb-3">
+            <div className="bg-white flex flex-col items-center py-8 px-10 md:px-20 shadow-custom mx-5 my-2 md:my-0">
+              <h3 className="text-heading3Size font-bold mb-3 text-center">
                 Jednorázová SMS
               </h3>
-              <p className="text-paragraphSize font-semibold">DMS MSSOZ 30</p>
-              <p className="text-paragraphSize font-semibold">DMS MSSOZ 60</p>
-              <p className="text-paragraphSize font-semibold">DMS MSSOZ 90</p>
-              <p className="text-paragraphSize font-bold mt-10">
+              <p className="text-paragraphSize font-semibold text-center">
+                DMS MSSOZ 30
+              </p>
+              <p className="text-paragraphSize font-semibold text-center">
+                DMS MSSOZ 60
+              </p>
+              <p className="text-paragraphSize font-semibold text-center">
+                DMS MSSOZ 90
+              </p>
+              <p className="text-paragraphSize font-bold mt-10 text-center">
                 Telefonní číslo: 87 777
               </p>
             </div>
             {/* Box */}
-            <div className="bg-white flex flex-col items-center py-8 px-20 shadow-custom mx-5">
-              <h3 className="text-heading3Size font-bold mb-3">Trvalá SMS</h3>
-              <p className="text-paragraphSize font-semibold">
+            <div className="bg-white flex flex-col items-center py-8 px-10 md:px-20 shadow-custom mx-5 my-2 md:my-0">
+              <h3 className="text-heading3Size font-bold mb-3 text-center">
+                Trvalá SMS
+              </h3>
+              <p className="text-paragraphSize font-semibold text-center">
                 DMS TRV MSSOZ 30
               </p>
-              <p className="text-paragraphSize font-semibold">
+              <p className="text-paragraphSize font-semibold text-center">
                 DMS TRV MSSOZ 60
               </p>
-              <p className="text-paragraphSize font-semibold">
+              <p className="text-paragraphSize font-semibold text-center">
                 DMS TRV MSSOZ 90
               </p>
-              <p className="text-paragraphSize font-bold mt-10">
+              <p className="text-paragraphSize font-bold mt-10 text-center">
                 Telefonní číslo: 87 777
               </p>
             </div>
           </div>
           {/* Lower container */}
-          <p className="text-paragraphSize mt-10">
-            Cena u jednorázové DMS je 30Kč, 60Kč nebo 90Kč...
+          <p className="text-paragraphSize mt-10 text-center">
+            Cena u jednorázové DMS je 30Kč, 60Kč nebo 90Kč. U trvalé DMS Vám
+            bude každý měsíc automaticky odečtena částka, kterou jste zvolili.
           </p>
         </div>
       </section>
@@ -119,10 +134,10 @@ function Home() {
       {/* Needs section */}
       <section className="bg-darkerSectionColor">
         {/* Responsive container */}
-        <div className="container mx-auto flex justify-center gap-x-20 py-10">
+        <div className="container mx-auto flex flex-col items-center md:flex-row md:justify-center md:items-start gap-x-10 lg:gap-x-20 py-10">
           {/* Left side */}
-          <div className="flex flex-col items-center w-2/5">
-            <h2 className="text-heading2Size font-bold mb-5">
+          <div className="flex flex-col items-center w-[90%] md:w-2/5">
+            <h2 className="text-heading2Size font-bold mb-5 text-center">
               Koho potřebujeme
             </h2>
             {/* Boxes generating */}
@@ -187,15 +202,17 @@ function Home() {
           </div>
 
           {/* Right side */}
-          <div className="flex flex-col items-center w-2/5">
-            <h2 className="text-heading2Size font-bold mb-5">Co potřebujeme</h2>
+          <div className="flex flex-col items-center w-[90%] md:w-2/5 mt-16 md:mt-0">
+            <h2 className="text-heading2Size font-bold mb-5 text-center">
+              Co potřebujeme
+            </h2>
             {/* Boxes generating */}
             {[
               {
                 icon: FaBowlRice,
                 title: 'Výživa',
                 description:
-                  'Kvalitní psí a kočičí granule (v žádném případě ne Propesko, Chappi, Pedigree, ...), to způsobuje zažívací problémy, které by se následně řešili veterinárně. Trainer, Ok Cat, Ok Dog, Felix kapsičky, Calibra, Platinum, Taste of Wild, Nutra Nuggets, Meowing Heads, Magnusson, Farm Fresh, Belcando, Topstein, Nutra Gold, Brit, Acana, Barking Heas, Purizon, Artemis, Orien, Applaws, Granada Pet Natural Taste, Carna4, Sam’s Field, Camilove, Purina Cat Chow. Dále masové psí a kočičí konzervy, kočičí kapsičky a konzervy Felix, jaterní granule a konzervy, ledvinové granule a konzervy, senior granule, krmné oleje, psí a kočičí pamlsky, kloubní výživu, jakékoliv vitamínové doplňky, sušený heřmánek nebo heřmánkový čaj.',
+                  'Kvalitní psí a kočičí granule (v žádném případě ne Propesko, Chappi, Pedigree, ...), to způsobuje zažívací problémy, které by se následně řešili veterinárně. Kvalitní granule jsou: Trainer, Ok Cat, Ok Dog, Felix kapsičky, Calibra, Platinum, Taste of Wild, Nutra Nuggets, Meowing Heads, Magnusson, Farm Fresh, Belcando, Topstein, Nutra Gold, Brit, Acana, Barking Heas, Purizon, Artemis, Orien, Applaws, Granada Pet Natural Taste, Carna4, Sam’s Field, Camilove, Purina Cat Chow. Dále masové psí a kočičí konzervy, kočičí kapsičky a konzervy Felix, jaterní granule a konzervy, ledvinové granule a konzervy, senior granule, krmné oleje, psí a kočičí pamlsky, kloubní výživu, jakékoliv vitamínové doplňky, sušený heřmánek nebo heřmánkový čaj.',
               },
               {
                 icon: FaPumpSoap,
